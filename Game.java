c20cac2795b1098de891cef388abffa6b43faf46
Skyla Thadani <skylathadani@MacBook-Pro.local>
2019-01-28 22:53:35 -0500

    fixed everything but two things

--- Game.java
+++ Game.java
@@ -13,12 +13,47 @@ public class Game {
 
 	public void play() {
 		int round = 1;
+		boolean y = true;
 		main.shuffle();
 		hand =  main.deal(7);
 		while(hand.size() > 0) {
+			
 			d.roll();
-			System.out.println("Welcome to round " + round + " you rolled a " + d.getValue() + ".");
-			if(d.getValue() == 1){
+			if(main.size()!=0){
+				System.out.println("Welcome to round " + round + " you rolled a " + d.getValue() + ".");
+
+			}else{
+				System.out.println("Welcome to round " + round); 
+				if(main.size()==0){
+					if(y == true){
+					y = Utils.readYesOrNo("Do you have any melds to discard?");
+				 	}
+				
+
+				hand.print();
+				System.out.println("Please choose a card to discard");
+				boolean b = false;
+				while(b == false){
+					Card c = Utils.readCard();
+					b = hand.remove(c);
+					if(b == false){
+						System.out.println("This card is not in the deck please try again");
+					}
+				}
+				round++;
+
+				
+
+
+			}
+
+			}
+			//System.out.println("Welcome to round " + round + " you rolled a " + d.getValue() + ".");
+
+			
+
+			
+			if(d.getValue() == 1 && main.size() !=0){
 				hand.print();
 				System.out.println("Please choose a card to discard");
 				boolean b = false;
@@ -31,7 +66,7 @@ public class Game {
 				}
 				round++;
 			}	
-			else{
+			else if(d.getValue() != 1 && main.size()!=0){
 				if(d.getValue() < main.size()){
 					for(int i = 0; i < d.getValue(); i++) {
 						Card temp = main.get(main.size() - 1);
@@ -75,10 +110,12 @@ public class Game {
 							}
 						}	
 					}
-					//contDiscard = Utils.readYesOrNo("Do you have any more melds to discard?");
+					
 				}
 				round++;
 			}
 		}
+
+		System.out.println("You have won the game in " + (round-1)+" rounds.");
 	}
 }

70abbb0f1a3e2bb45a1bb90654e16e8dc950fbdb
GregMitch <grmitch1@gmail.com>
2019-02-01 18:02:18 -0500

    Added java docs to A1Q2 and A1Q3.

--- 01/A1Q2.java
+++ 01/A1Q2.java
@@ -1,16 +1,24 @@
 /**
-* Author: Gregory Mitchell
+* @author: Gregory Mitchell
 * Student number: 300053307
 * Course: ITI 1121-A00
 * Assignment: 1
 
-* Author: Skyla Thadani
+* @author: Skyla Thadani
 * Student number: 300055455
 * Course: ITI 1121-C00
 * Assignment: 1
 */
 public class A1Q2{
+/**
+* @author: Gregory Mitchell
+* @author: Skyla Thadani
+*/
 	public static boolean hasLengthTwoRun(double[] ar){
+		/**
+		* @param ar represents the double array given as input to the method.
+		* @return returns boolean value true if there is at least a run of two cards and false otherwise.
+		*/
 
 		for(int i = 0; i < ar.length-1;i++){
 			//if there is a run of two -> return true and program will terminate
--- 01/A1Q3.java
+++ 01/A1Q3.java
@@ -1,20 +1,24 @@
 /**
-* Author: Gregory Mitchell
+* @author: Gregory Mitchell
 * Student number: 300053307
 * Course: ITI 1121-A00
 * Assignment: 1
 
-* Author: Skyla Thadani
+* @author: Skyla Thadani
 * Student number: 300055455
 * Course: ITI 1121-C00
 * Assignment: 1
 */
 public class A1Q3{
-
+/**
+* @author: Gregory Mitchell
+* @author: Skyla Thadani
+*/
 	public static int getLongestRun(double ar[]){
-		//this is for arrays that are type double
-
-		//this is a temporary variable (needs to start at one because the smallest run will always be one)
+		/**
+		* @param ar double array 
+		* @return maxCount, which is the length of the longest run if the array has a length > 1. Other wise returns 0. 
+		*/
 		int tmp = 1;
 		//this is the counting variable
 		int count = 1;
@@ -51,23 +55,34 @@ public class A1Q3{
 	}
 
 	public static int getLongestRun(String ar[]){
+		/**
+		* @param ar String array 
+		* @return maxCount, which is the length of the longest run if the array has a length > 1. Otherwise returns 0.
+		*/
+		
+		//this is a temporary variable (needs to start at one because the smallest run will always be one)
 		int tmp = 1;
+		//this is the counting variable
 		int count = 1;
+		//holds the value of the maximum count
 		int maxCount = 1;
 
 		if(ar.length == 0) {
 			return 0;
 		}
 
+		// Checks if element in front of current element is the same and increments count if they are.
 		for(int i = 0; i < ar.length-1;i++){
 			if(ar[i].equals(ar[i+1])){
 				count ++;
 			}
 
+			// sets count back to one after run has been broken.
 			if(ar[i] != ar[i+1]){
 				tmp = count;
 				count = 1;
 
+				// sets maxCount to length of longest run found in array.
 				if(tmp > maxCount){
 					maxCount = tmp;
 				}

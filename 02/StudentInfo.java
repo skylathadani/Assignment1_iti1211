c5301eb286626ba3ec104108ab4d39b10a46f80a
Skyla Thadani <skylathadani@MacBook-Pro.local>
2019-02-01 13:54:39 -0500

    moved files around

---
+++ 02/StudentInfo.java
@@ -0,0 +1,19 @@
+//@author Marcel Turcotte (marcel.turcotte@uottawa.ca)
+public class StudentInfo {
+	public static void display() {
+        System.out.println("************************************************************");
+        System.out.println("*Student Name: Gregory Micthell                            *");
+        System.out.println("*Student ID:   300053307                                   *");
+        System.out.println("*Section:      ITI 1121 A00                                *");
+        System.out.println("*                                                          *");
+        System.out.println("************************************************************");
+        System.out.println();
+        System.out.println("************************************************************");
+        System.out.println("*Student Name: Skyla Thadani                               *");
+        System.out.println("*Student ID:   300055455                                   *");
+        System.out.println("*Section:      ITI 1121 C00                                *");
+        System.out.println("*                                                          *");
+        System.out.println("************************************************************");
+        System.out.println();
+    }
+}
--- README.md
+++
@@ -1,7 +0,0 @@
-# Assignment1_iti1211
-
-Skyla Thadani
-300055455
-
-Gregory Mitchell
-300053307
